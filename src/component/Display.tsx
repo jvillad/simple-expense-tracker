b73318d07e1403fe5a1bea3e18b8905c936c6fe6
jvillad <jyvillad@gmail.com>
2023-01-19 17:23:55 +1100

    pure display component

--- src/component/Display.tsx
+++ src/component/Display.tsx
@@ -4,18 +4,16 @@ interface ExpenseList {
     description: string;
     category: string;
   };
-  dailyBudget: number;
+  userBalance: number;
 }
 
-function Display({ expenseList, dailyBudget }: ExpenseList) {
+function Display({ expenseList, userBalance }: ExpenseList) {
   return (
     <div className="grid grid-cols-6 gap-3 max-w-3xl m-auto pt-10">
       <div className="col-span-2">{expenseList.category}</div>
       <div>{expenseList.expense}</div>
       <div className="col-span-2">{expenseList.description}</div>
-      <div>{`You have $${
-        dailyBudget - expenseList.expense
-      } remaining on your budget`}</div>
+      <div>{`You have $${userBalance} remaining on your budget`}</div>
     </div>
   );
 }
