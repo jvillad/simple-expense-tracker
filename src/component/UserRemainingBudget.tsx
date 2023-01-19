5193d0a9f8e71ae38eb6f4d0d6a44d94a20fec7d
jvillad <jyvillad@gmail.com>
2023-01-19 17:25:17 +1100

    component to display remaining budget

---
+++ src/component/UserRemainingBudget.tsx
@@ -0,0 +1,14 @@
+interface IProps {
+  remainingBudget: number;
+}
+
+function UserBudgetHeader({ remainingBudget }: IProps) {
+  return (
+    <div>
+      <h1 className="text-center text-lg py-4 font-bold text-[#011627]">
+        {`Remaining Budget: $${remainingBudget}`}
+      </h1>
+    </div>
+  );
+}
+export default UserBudgetHeader;
