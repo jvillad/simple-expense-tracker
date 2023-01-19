45d22410c3735e4109f8df9aedfd7101ce589385
jvillad <jyvillad@gmail.com>
2023-01-19 17:25:06 +1100

    component to display max budget

---
+++ src/component/UserBudgetHeader.tsx
@@ -0,0 +1,14 @@
+interface MaxBudget {
+  userDailyBudget: number;
+}
+
+function UserBudgetHeader({ userDailyBudget }: MaxBudget) {
+  return (
+    <div>
+      <h1 className="text-center text-lg py-4 font-bold text-[#011627]">
+        {`Budget for today is: $${userDailyBudget}`}
+      </h1>
+    </div>
+  );
+}
+export default UserBudgetHeader;
