d459f870fc226ddb3ab1e814e0e6e9c1f3516321
jvillad <jyvillad@gmail.com>
2023-01-20 20:52:04 +1100

    refactor, remove user balance in display

--- src/component/Display.tsx
+++ src/component/Display.tsx
@@ -4,16 +4,14 @@ interface ExpenseList {
     description: string;
     category: string;
   };
-  userBalance: number;
 }
 
-function Display({ expenseList, userBalance }: ExpenseList) {
+function Display({ expenseList }: ExpenseList) {
   return (
-    <div className="grid grid-cols-6 gap-3 max-w-3xl m-auto pt-10">
+    <div className="grid grid-cols-4 gap-3 max-w-3xl m-auto pt-10">
       <div className="col-span-2">{expenseList.category}</div>
       <div>{expenseList.expense}</div>
-      <div className="col-span-2">{expenseList.description}</div>
-      <div>{`You have $${userBalance} remaining on your budget`}</div>
+      <div>{expenseList.description}</div>
     </div>
   );
 }
