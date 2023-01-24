5949101cb1954ef3026c3bb65966840174cc9eb3
jvillad <jyvillad@gmail.com>
2023-01-24 14:11:14 +1100

    import Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import MainForm from './component/MainForm';
 import Header from './component/Header';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <MainForm />
+      <Footer />
     </div>
   );
 }
