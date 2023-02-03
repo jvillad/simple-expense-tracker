fd3c7f4397009aa48da0afdd3a2202a952a5bc3a
jvillad <jyvillad@gmail.com>
2023-02-03 20:03:03 +1100

    remaining budget hidden if no budget

--- src/component/UserRemainingBudget.tsx
+++ src/component/UserRemainingBudget.tsx
@@ -1,12 +1,19 @@
 interface IProps {
-  remainingBudget: number | string | null;
+  remainingBudget: number | null;
 }
 
 function UserBudgetHeader({ remainingBudget }: IProps) {
   return (
     <div>
-      <h1 className="text-center text-lg py-4 font-bold text-[#011627]">
-        {`Remaining Budget: $${remainingBudget}`}
+      <h1
+        className={
+          remainingBudget
+            ? 'text-center text-lg py-4 font-bold text-[#011627]'
+            : 'hidden'
+        }
+      >
+        Remaining Budget:{' '}
+        <span className="text-[#f353b2]">${remainingBudget}</span>
       </h1>
     </div>
   );
